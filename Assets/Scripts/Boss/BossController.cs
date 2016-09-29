d7d12e6eb76e26e3e4bcdeb69d0f7ec5bb4d0f97
unknown <jwon223@aucklanduni.ac.nz>
2016-09-29 18:31:36 +1300

    added jump

--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -14,7 +14,20 @@ public class BossController : MonoBehaviour {
     public double currentHealth = 100;
     public double threshold = 0.5;
 
+	public double chargeDuration = 1;
+	public double chargeStartTime = 1;
+
+	private float ypos = 0;
+	private float time;
+
+	private float verticalSpeed = 0;
+	private float horizontalSpeed = 0;
+	private float zSpeed = 0;
+	private const double gravity = -9.81;
+	private bool jumping = false;
+
     private Vector3 offset = new Vector3(2, 0, 0);
+
     // Use this for initialization
     void Start () {
        currentHealth = maxHealth;
@@ -22,8 +35,45 @@ public class BossController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //enemyPosition.rotation = Quaternion.Slerp(enemyPosition.rotation, Quaternion.LookRotation(playerPosition.position - enemyPosition.position), rotationSpeed * Time.deltaTime);
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.05F);
+		//enemy.transform.rotation = Quaternion.Slerp(enemy.transform.rotation, Quaternion.LookRotation(this.transform.position - enemy.transform.position), rotationSpeed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.05F);
+
+		if (Time.fixedTime > (chargeStartTime + chargeDuration) && !jumping) {
+			chargeStartTime = Time.fixedTime;
+			verticalSpeed = (float)2;
+
+
+			//float temp = this.transform.position.x - player.transform.position.x;
+			//horizontalSpeed = this.transform.position.z - player.transform.position.z;
+			//horizontalSpeed = Mathf.Sqrt (Mathf.Pow(temp,2) + Mathf.Pow(horizontalSpeed,2));
+			//zSpeed = Mathf.Abs( this.transform.position.z - player.transform.position.z);
+			//horizontalSpeed = Mathf.Abs(this.transform.position.x - player.transform.position.x);
+			jumping = true;
+		}
+
+		if (jumping) {
+			//this.transform.position.y
+
+			time = (float)Time.deltaTime;
+			verticalSpeed = verticalSpeed + (float)gravity * time;
+			//horizontalSpeed = horizontalSpeed * (float)Time.deltaTime;
+			//zSpeed = zSpeed * (float)Time.deltaTime;
+
+			ypos = ypos + (verticalSpeed*time);
+			print (ypos);
+			if (ypos < 0) {
+				verticalSpeed = 2;
+				ypos = 0;
+				jumping = false;
+			}
+
+			this.transform.Translate (0, ypos, 0);
+		
+			//if (verticalSpeed <= 0) {
+			//	jumping = false;
+			//}
+		}
+
         if (currentLevel < maxLevel && currentHealth <= maxHealth * threshold)
         {
             GameObject child1 = (GameObject)Instantiate(enemy, this.transform.position + offset, Quaternion.identity);
@@ -47,13 +97,4 @@ public class BossController : MonoBehaviour {
         }
     }
 
-    void Awake()
-    {
-        InvokeRepeating("JumpCharge", 2, 2);
-    }
-
-    void JumpCharge()
-    {
-        
-    }
 }

3a37b3c00796670996c8426fa2fa56ecb9f38905
JSCooke <jaydencooke@outlook.com>
2016-09-30 14:18:00 +1300

    Committed non-binary version of health bar files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+//Adapted from inScope Studios' health bar tutorial
+public class Player : MonoBehaviour {
+
+	public Stat health;
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			health.CurrentVal += 10;
+		} else if (Input.GetKeyDown (KeyCode.S)) {
+			health.CurrentVal -= 10;
+		} else if (Input.GetKeyDown (KeyCode.Space)) {
+			health.ObjectiveCount += 1;
+		}
+	}
+
+//	float Damage(float damage) {
+//		health.CurrentVal -= damage;
+//	}
+
+}
---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+//Adapted from inScope Studios' health bar tutorial
+using UnityEngine.UI;
+
+
+[Serializable]
+public class Stat
+{
+	public BarScript bar;
+	public Text keys;
+	public float currentVal;
+	public float objectiveCount;
+	public float objectiveMax;
+
+
+	public float CurrentVal {
+		get {
+			return currentVal;
+		}
+		set { 
+			currentVal = Mathf.Clamp(value, 0, 100);
+			bar.Value = currentVal;
+		}
+	}
+
+	public float ObjectiveCount {
+		get {
+			return objectiveCount;
+		}
+		set {
+			float newValue = Mathf.Clamp (value, 0, objectiveMax);
+			string[] tmp = keys.text.Split(new Char [] {' ' , '/' });
+			keys.text = tmp [0] + " " + newValue + "/" + objectiveMax; 
+			objectiveCount = newValue;
+		}
+	}
+}

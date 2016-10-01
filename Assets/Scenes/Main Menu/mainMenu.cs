ac166735e8652af569db2b0eb139cdd1f29d2414
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-01 14:49:08 +1300

    Added buttons to main menu and quit menu

---
+++ Assets/Scenes/Main Menu/mainMenu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    //Use this when "Play" button is pressed
+    public void Play()
+    {
+
+    }
+
+    //Use this when "Leaderboard" is pressed
+    public void LeaderboardPress()
+    {
+
+    }
+
+    //Use this when "Customise" is pressed
+    public void CustomisePress()
+    {
+
+    }
+
+    //Use this when "Achievements" is pressed
+    public void AchievementsPress()
+    {
+
+    }
+
+    //Use this when "Settings" is pressed
+    public void SettingsPress()
+    {
+
+    }
+
+    //Use this when "Exit" is pressed
+    public void ExitPress()
+    {
+
+    }
+
+    //Use this when "No" is pressed after "Exit"
+    public void NoPress()
+    {
+
+    }
+
+    //Use this when "Yes" is pressed after "Exit"
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}

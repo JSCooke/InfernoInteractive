95f8976d4aa98bdbce6c5e808f5f476b7c0e89bc
JSCooke <jaydencooke@outlook.com>
2016-10-03 12:03:58 +1300

    Corrected error caused by not all files being committed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,28 @@ using System;
 //Adapted from inScope Studios' health bar tutorial
 //This demonstrates the code  on the player's side.
 using UnityEngine.UI;
+using AssemblyCSharp;
 
 
 public class Player : MonoBehaviour {
 
-	public Stat health;
-	public Stat boss;
-	public Animator achievementAnimator;
-	public Image achievementBox;
+	public UIAdapter ui;
 	// Use this for initialization
 	void Start () {
-		achievementAnimator = achievementBox.GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.W)) {
-			health.damage (-10);
+			ui.damagePlayer (-10);
 		} else if (Input.GetKeyDown (KeyCode.S)) {
-			health.damage (10);
+			ui.damagePlayer (10);
 		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			boss.damage (-1);
+			ui.damageBoss (-1);
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			boss.damage (1);
+			ui.damageBoss (1);
 		} else if (Input.GetKeyDown (KeyCode.Space)) {
-			achievementAnimator.SetTrigger ("Achievement");
+			ui.achieve();
 		}
 	}
 }
--- Assets/Scripts/UIAdapter.cs
+++ Assets/Scripts/UIAdapter.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 namespace AssemblyCSharp
 {
+	[Serializable]
 	public class UIAdapter
 	{
 		public Stat health;

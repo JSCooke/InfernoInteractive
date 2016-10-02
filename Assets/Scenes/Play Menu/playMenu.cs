fe78e2afe27bb25704858985b8e0644c02f11382
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-02 22:28:16 +1300

    Added level preview on hover

--- Assets/Scenes/Play Menu/playMenu.cs
+++ Assets/Scenes/Play Menu/playMenu.cs
@@ -1,8 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class playMenu : MonoBehaviour {
+
+    public Image Preview;
+    public Text Desciption;
+
+    public Sprite[] levelSprites = new Sprite[5];
+    string[] descriptions = new string[5];
+
+    void Start()
+    {
+        SetDescriptions();
+    }
 
     public void LoadScene(int level)
     {
@@ -12,5 +24,20 @@ public class playMenu : MonoBehaviour {
     public void BackPress()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void ShowPreview(int level)
+    {
+        Preview.sprite = levelSprites[level - 1];
+        Desciption.text = descriptions[level - 1];
+    }
+
+    void SetDescriptions()
+    {
+        descriptions[0] = "Defeat Slime Boss";
+        descriptions[1] = "Pirate Level";
+        descriptions[2] = "Navigate the Maze";
+        descriptions[3] = "Separated";
+        descriptions[4] = "Final Chase";
     }
 }

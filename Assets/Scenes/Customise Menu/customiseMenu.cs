ddedaa0b8e72c54cdc53e6795800badba8a512be
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-02 19:10:13 +1300

    Customise menu implemented
    Clicking on various upgrades displays changes on tank

--- Assets/Scenes/Customise Menu/customiseMenu.cs
+++ Assets/Scenes/Customise Menu/customiseMenu.cs
@@ -4,11 +4,34 @@ using UnityEngine.SceneManagement;
 
 public class customiseMenu : MonoBehaviour {
 
-    public GameObject[] upgradeObjects = new GameObject[7];
+    public GameObject[] AttackUpgrades = new GameObject[2];
+    public GameObject[] DefenseUpgrades = new GameObject[1];
+    public GameObject[] MovementUpgrades = new GameObject[0];
+    public GameObject[] LeftUpgrades = new GameObject[2];
+    public GameObject[] RightUpgrades = new GameObject[2];
 
     void Start()
     {
-
+        for (int i = 0; i < AttackUpgrades.Length; i++)
+        {
+            AttackUpgrades[i].SetActive(false);
+        }
+        for (int i = 0; i < DefenseUpgrades.Length; i++)
+        {
+            DefenseUpgrades[i].SetActive(false);
+        }
+        for (int i = 0; i < MovementUpgrades.Length; i++)
+        {
+            MovementUpgrades[i].SetActive(false);
+        }
+        for (int i = 0; i < LeftUpgrades.Length; i++)
+        {
+            LeftUpgrades[i].SetActive(false);
+        }
+        for (int i = 0; i < RightUpgrades.Length; i++)
+        {
+            RightUpgrades[i].SetActive(false);
+        }
     }
 
     public void BackPress()
@@ -16,8 +39,48 @@ public class customiseMenu : MonoBehaviour {
         SceneManager.LoadScene("Main");
     }
 
-    public void UpgradeShow(GameObject upgradeObject)
+    public void AttackUpgradeShow(GameObject upgradeObject)
     {
+        for (int i = 0; i < AttackUpgrades.Length; i++)
+        {
+            AttackUpgrades[i].SetActive(false);
+        }
+        upgradeObject.SetActive(true);
+    }
 
+    public void DefenseUpgradeShow(GameObject upgradeObject)
+    {
+        for (int i = 0; i < DefenseUpgrades.Length; i++)
+        {
+            DefenseUpgrades[i].SetActive(false);
+        }
+        upgradeObject.SetActive(true);
+    }
+
+    public void MovementUpgradeShow(GameObject upgradeObject)
+    {
+        for (int i = 0; i < MovementUpgrades.Length; i++)
+        {
+            MovementUpgrades[i].SetActive(false);
+        }
+        upgradeObject.SetActive(true);
+    }
+
+    public void LeftUpgradeShow(GameObject upgradeObject)
+    {
+        for (int i = 0; i < LeftUpgrades.Length; i++)
+        {
+            LeftUpgrades[i].SetActive(false);
+        }
+        upgradeObject.SetActive(true);
+    }
+
+    public void RightUpgradeShow(GameObject upgradeObject)
+    {
+        for (int i = 0; i < RightUpgrades.Length; i++)
+        {
+            RightUpgrades[i].SetActive(false);
+        }
+        upgradeObject.SetActive(true);
     }
 }

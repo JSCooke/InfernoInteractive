51606fa509df1da43c931c0ef8e74a73e9b18cf8
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-02 13:46:18 +1300

    Attempting to display list of upgrades...
    ...depending on which control station is pressed.

---
+++ Assets/Scenes/Customise Menu/UpgradePanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpgradePanel : MonoBehaviour
+{
+    public Text Name;
+    public Image Image;
+
+    public void setName(string newName)
+    {
+        Name.text = newName;
+    }
+}
+
--- Assets/Scenes/Customise Menu/upgradeSelect.cs
+++ Assets/Scenes/Customise Menu/upgradeSelect.cs
@@ -1,9 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class upgradeSelect : MonoBehaviour {
 
 	public GameObject Transform;
+    public GameObject UpgradePanel;
 
+    //public Text[] attackNames;
+    //public Text[] defenseNames;
+    //public Text[] otherNames;
+
+    public Upgrade[] attackUpgrades = new Upgrade[2];
+    public Upgrade[] defenseUpgrades = new Upgrade[1];
+    public Upgrade[] movementUpgrades = new Upgrade[1];
+    public Upgrade[] otherUpgrades = new Upgrade[1];
+
+    void Start()
+    {
+
+    }
+
+
+    public void DisplayAttackUpgrades()
+    {
+        Instantiate(UpgradePanel, new Vector3(transform.position.x, transform.position.y), new Quaternion());
+    }
+
+    public void DisplayDefenseUpgrades()
+    {
+
+    }
+
+    public void DisplayMovementUpgrades()
+    {
+
+    }
+
+    public void DisplayOtherUpgrades()
+    {
+
+    }
+
+    public class Upgrade
+    {
+        public int StationType;
+        public string Name;
+        public Image Image;
+        public GameObject WeaponObject;
+    }
 
 }

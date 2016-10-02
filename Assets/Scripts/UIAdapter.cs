e43d6924831347dbbd3966dc156e4429337bb6d9
JSCooke <jaydencooke@outlook.com>
2016-10-03 11:52:03 +1300

    Created UIAdapter class to simplify player side code.

---
+++ Assets/Scripts/UIAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AssemblyCSharp
+{
+	public class UIAdapter
+	{
+		public Stat health;
+		public Stat boss;
+		public Animator achievementAnimator;
+		public Image achievementBox;
+		public UIAdapter ()
+		{
+			achievementAnimator = achievementBox.GetComponent<Animator>();
+		}
+		//Reduces (negative increases) the player's health by the input percentage.
+		//Returns the remaining hp of the player.
+		public float damagePlayer(float hp){
+			health.damage (hp);
+			return health.CurrentVal;
+		}
+		//Reduces (negative increases) the boss's health by the input percentage.
+		//Returns the remaining hp of the boss.
+		public float damageBoss(float hp){
+			boss.damage (hp);
+			return boss.CurrentVal;
+		}
+		//Causes an achievement box to pop up
+		//Add paramters to specify details about the achievement.
+		public void achieve(){
+			achievementAnimator.SetTrigger ("Achievement");
+		}
+	}
+}
+

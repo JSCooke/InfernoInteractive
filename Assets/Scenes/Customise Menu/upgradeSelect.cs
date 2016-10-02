17391f9128c329aa3a29b6917c7c1a7ca43f0347
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-02 14:17:34 +1300

    Fixed layout of upgrade panels to vertical

--- Assets/Scenes/Customise Menu/upgradeSelect.cs
+++ Assets/Scenes/Customise Menu/upgradeSelect.cs
@@ -18,28 +18,69 @@ public class upgradeSelect : MonoBehaviour {
 
     void Start()
     {
-
+        FillAttackList();
+        FillDefenseList();
+        FillMovementList();
+        FillOtherList();
     }
 
 
     public void DisplayAttackUpgrades()
     {
-        Instantiate(UpgradePanel, new Vector3(transform.position.x, transform.position.y), new Quaternion());
+        for (int i = 0; i < attackUpgrades.Length; i++)
+        {
+            
+        }
     }
 
     public void DisplayDefenseUpgrades()
     {
-
+        for (int i = 0; i < defenseUpgrades.Length; i++)
+        {
+           
+        }
     }
 
     public void DisplayMovementUpgrades()
     {
-
+        for (int i = 0; i < movementUpgrades.Length; i++)
+        {
+            
+        }
     }
 
     public void DisplayOtherUpgrades()
     {
+        for (int i = 0; i < otherUpgrades.Length; i++)
+        {
+           
+        }
+    }
 
+    void FillAttackList()
+    {
+        attackUpgrades[0] = new Upgrade();
+        attackUpgrades[0].Name = "Cannon";
+
+        attackUpgrades[1] = new Upgrade();
+        attackUpgrades[1].Name = "Chainsaw";
+    }
+
+    void FillDefenseList()
+    {
+        defenseUpgrades[0] = new Upgrade();
+        defenseUpgrades[0].Name = "Shield";
+    }
+
+    void FillMovementList()
+    {
+        
+    }
+
+    void FillOtherList()
+    {
+        otherUpgrades[0] = new Upgrade();
+        otherUpgrades[0].Name = "Ray Gun";
     }
 
     public class Upgrade

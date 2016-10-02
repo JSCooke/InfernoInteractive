e5e0f5f03caa6ac8ebed0ff8008d2feeed5a1500
JSCooke <jaydencooke@outlook.com>
2016-10-03 11:38:08 +1300

    Added achievement animation that appears on trigger

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,26 +3,32 @@ using System.Collections;
 using System;
 //Adapted from inScope Studios' health bar tutorial
 //This demonstrates the code  on the player's side.
+using UnityEngine.UI;
+
+
 public class Player : MonoBehaviour {
 
 	public Stat health;
 	public Stat boss;
+	public Animator achievementAnimator;
+	public Image achievementBox;
 	// Use this for initialization
 	void Start () {
+		achievementAnimator = achievementBox.GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.W)) {
-			health.damage(-10);
+			health.damage (-10);
 		} else if (Input.GetKeyDown (KeyCode.S)) {
-			health.damage(10);
-		} else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			health.damage (10);
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			boss.damage (-1);
-		} else if (Input.GetKeyDown(KeyCode.DownArrow)){
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			boss.damage (1);
+		} else if (Input.GetKeyDown (KeyCode.Space)) {
+			achievementAnimator.SetTrigger ("Achievement");
 		}
-
 	}
-
 }

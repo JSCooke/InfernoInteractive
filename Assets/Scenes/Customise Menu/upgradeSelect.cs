0141f16a4aafc4cd453192fa91c83c27bd9f4c96
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-02 14:53:11 +1300

    Station Select displays appropriate upgrades

--- Assets/Scenes/Customise Menu/upgradeSelect.cs
+++ Assets/Scenes/Customise Menu/upgradeSelect.cs
@@ -4,91 +4,53 @@ using System.Collections;
 
 public class upgradeSelect : MonoBehaviour {
 
-	public GameObject Transform;
-    public GameObject UpgradePanel;
-
-    //public Text[] attackNames;
-    //public Text[] defenseNames;
-    //public Text[] otherNames;
-
-    public Upgrade[] attackUpgrades = new Upgrade[2];
-    public Upgrade[] defenseUpgrades = new Upgrade[1];
-    public Upgrade[] movementUpgrades = new Upgrade[1];
-    public Upgrade[] otherUpgrades = new Upgrade[1];
+    public GameObject AttackStation;
+    public GameObject DefenseStation;
+    public GameObject MovementStation;
+    public GameObject OtherStation;
 
     void Start()
     {
-        FillAttackList();
-        FillDefenseList();
-        FillMovementList();
-        FillOtherList();
-    }
+        //AttackStation = AttackStation.GetComponent<Image>();
+        //DefenseStation = DefenseStation.GetComponent<Image>();
+        //MovementStation = MovementStation.GetComponent<Image>();
+        //OtherStation = OtherStation.GetComponent<Image>();
 
+        AttackStation.SetActive(true);
+        DefenseStation.SetActive(false);
+        MovementStation.SetActive(false);
+        OtherStation.SetActive(false);
+    }
 
     public void DisplayAttackUpgrades()
     {
-        for (int i = 0; i < attackUpgrades.Length; i++)
-        {
-            
-        }
+        AttackStation.SetActive(true);
+        DefenseStation.SetActive(false);
+        MovementStation.SetActive(false);
+        OtherStation.SetActive(false);
     }
 
     public void DisplayDefenseUpgrades()
     {
-        for (int i = 0; i < defenseUpgrades.Length; i++)
-        {
-           
-        }
+        AttackStation.SetActive(false);
+        DefenseStation.SetActive(true);
+        MovementStation.SetActive(false);
+        OtherStation.SetActive(false);
     }
 
     public void DisplayMovementUpgrades()
     {
-        for (int i = 0; i < movementUpgrades.Length; i++)
-        {
-            
-        }
+        AttackStation.SetActive(false);
+        DefenseStation.SetActive(false);
+        MovementStation.SetActive(true);
+        OtherStation.SetActive(false);
     }
 
     public void DisplayOtherUpgrades()
     {
-        for (int i = 0; i < otherUpgrades.Length; i++)
-        {
-           
-        }
+        AttackStation.SetActive(false);
+        DefenseStation.SetActive(false);
+        MovementStation.SetActive(false);
+        OtherStation.SetActive(true);
     }
-
-    void FillAttackList()
-    {
-        attackUpgrades[0] = new Upgrade();
-        attackUpgrades[0].Name = "Cannon";
-
-        attackUpgrades[1] = new Upgrade();
-        attackUpgrades[1].Name = "Chainsaw";
-    }
-
-    void FillDefenseList()
-    {
-        defenseUpgrades[0] = new Upgrade();
-        defenseUpgrades[0].Name = "Shield";
-    }
-
-    void FillMovementList()
-    {
-        
-    }
-
-    void FillOtherList()
-    {
-        otherUpgrades[0] = new Upgrade();
-        otherUpgrades[0].Name = "Ray Gun";
-    }
-
-    public class Upgrade
-    {
-        public int StationType;
-        public string Name;
-        public Image Image;
-        public GameObject WeaponObject;
-    }
-
 }

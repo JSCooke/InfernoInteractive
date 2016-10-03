592c17060fdfb68101e63320a776a6ffd96b3bab
Kristy Tsoi <kris.t.tsoi@gmail.com>
2016-10-03 21:28:58 +1300

    scripts to switch between scenes

---
+++ Assets/Scripts/objectiveMenuBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class objectiveMenuBehaviour : MonoBehaviour {
+    public Canvas objectiveMenu;
+    public Button returnBtn;
+    public Text objectiveDescription;
+
+    // Use this for initialization
+    void Start () {
+        objectiveMenu = objectiveMenu.GetComponent<Canvas>();
+        returnBtn = returnBtn.GetComponent<Button>();
+        objectiveDescription = objectiveDescription.GetComponent<Text>();
+        objectiveMenu.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void pressReturn()
+    {
+        Application.LoadLevel("PauseMenu");
+    }
+}
---
+++ Assets/Scripts/objectiveTextPass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class objectiveTextPass : MonoBehaviour {
+
+    static string objectiveDescription = "";
+    Text objeText;
+
+	// Use this for initialization
+	void Start () {
+        objeText.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        objeText.text = objectiveDescription;
+	}
+}

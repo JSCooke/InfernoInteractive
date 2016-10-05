d4549ed70514485bf8ce756946fce63185cca223
Hanzhi Wang <hanzhi2@gmail.com>
2016-10-05 18:20:36 +1300

    changing difficulty in menu is now reflected in actual level

--- Assets/Scripts/MenuScripts/SettingsMenu.cs
+++ Assets/Scripts/MenuScripts/SettingsMenu.cs
@@ -10,4 +10,15 @@ public class SettingsMenu : MonoBehaviour {
 		this.GetComponent<Canvas> ().enabled = false;
 		mainMenu.enabled = true;
 	}
+    public void setDifficulty(string difficulty) {
+        if (difficulty == "easy") {
+            GameData.put("difficulty",BossController.Difficulty.Easy);
+        }
+        if (difficulty == "medium") {
+            GameData.put("difficulty", BossController.Difficulty.Medium);
+        }
+        if (difficulty == "hard") {
+            GameData.put("difficulty", BossController.Difficulty.Hard);
+        }
+    }
 }

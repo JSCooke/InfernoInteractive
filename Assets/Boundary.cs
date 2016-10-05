4551621bc57e99c81732c0125297dc47c57df249
Jordan <jwon223@aucklanduni.ac.nz>
2016-10-05 19:37:17 +1300

    added boundary around boss area

---
+++ Assets/Boundary.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour {
+
+    public string projectile;
+
+    void OnTriggerExit(Collider other) {
+        print(other.gameObject.tag);
+        if (other.gameObject.tag == projectile) {
+            Destroy(other.gameObject);
+        }   
+    }
+}

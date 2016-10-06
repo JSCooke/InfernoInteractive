e9b0ce0cbd3f82e028374a2f991bb456096b3ca1
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-07 00:35:44 +1300

    Added comments to my code

--- Assets/Scripts/MenuScripts/playMenu.cs
+++ Assets/Scripts/MenuScripts/playMenu.cs
@@ -4,11 +4,12 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class playMenu : MonoBehaviour {
-
+	
     public Image Preview;
     public Text Desciption;
 	public Canvas mainMenu;
 
+	//Arrays of sprites and descriptions cooresponding to levels
     public Sprite[] levelSprites = new Sprite[5];
     string[] descriptions = new string[5];
 
@@ -19,24 +20,28 @@ public class playMenu : MonoBehaviour {
 
     public void LoadScene(int level)
     {
+		//Each level button calls this function to load the appropriate scene for that level
 		print (level);
         SceneManager.LoadScene(level+1);
     }
 
     public void BackPress()
     {
+		//Shows main menu
 		this.GetComponent<Canvas> ().enabled = false;
 		mainMenu.enabled = true;
     }
 
     public void ShowPreview(int level)
     {
+		//Shows preview of sprite and description of level on hover
         Preview.sprite = levelSprites[level - 1];
         Desciption.text = descriptions[level - 1];
     }
 
     void SetDescriptions()
     {
+		//List of level descriptions
         descriptions[0] = "Defeat Slime Boss";
         descriptions[1] = "Pirate Level";
         descriptions[2] = "Navigate the Maze";

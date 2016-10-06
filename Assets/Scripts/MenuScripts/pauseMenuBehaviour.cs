78d7d6de0aa4ecd7d217949bb6aea908e4914cf2
Hanzhi Wang <hanzhi2@gmail.com>
2016-10-06 22:14:49 +1300

    fixed pause menu not being disabled properly after pausing then unpausing. resolves #31

--- Assets/Scripts/MenuScripts/pauseMenuBehaviour.cs
+++ Assets/Scripts/MenuScripts/pauseMenuBehaviour.cs
@@ -16,7 +16,9 @@ public class pauseMenuBehaviour : MonoBehaviour {
 		objectives.enabled = false;
 		controls.enabled = false;
 		this.GetComponent<Canvas> ().enabled = false;
-
+        foreach(Transform t in transform) {
+            t.gameObject.SetActive(false);
+        }
     }
 	
 	// Update is called once per frame
@@ -24,23 +26,36 @@ public class pauseMenuBehaviour : MonoBehaviour {
         if (Input.GetKeyDown("escape"))
         {
 			if (this.GetComponent<Canvas> ().isActiveAndEnabled) {
-				//UNPAUSE
-				this.GetComponent<Canvas> ().enabled = false;
-                Time.timeScale = 1;
+                Unpause();
 			} else {
-				//PAUSE
-				if (controls.isActiveAndEnabled || objectives.isActiveAndEnabled) {
-					objectives.enabled = false;
-					controls.enabled = false;
-					this.GetComponent<Canvas> ().enabled = true;
-                    Time.timeScale = 0;
-                } else {
-					this.GetComponent<Canvas> ().enabled = true;
-                    Time.timeScale = 0;
-                }
-
+                Pause();
 			}
+        }
+    }
 
+    public void Pause() {
+        if (controls.isActiveAndEnabled || objectives.isActiveAndEnabled) {
+            objectives.enabled = false;
+            controls.enabled = false;
+            this.GetComponent<Canvas>().enabled = true;
+            Time.timeScale = 0;
+        } else {
+            this.GetComponent<Canvas>().enabled = true;
+            Time.timeScale = 0;
+        }
+        //enable all pause menu buttons
+        foreach (Transform t in transform) {
+            t.gameObject.SetActive(true);
+        }
+    }
+
+    public void Unpause() {
+        this.GetComponent<Canvas>().enabled = false;
+        Time.timeScale = 1;
+
+        //disable all pause menu buttons
+        foreach (Transform t in transform) {
+            t.gameObject.SetActive(false);
         }
     }
 
@@ -64,9 +79,7 @@ public class pauseMenuBehaviour : MonoBehaviour {
 
     public void resumePress()
     {
-		//UNPAUSE
-		Time.timeScale = 1;
-		this.GetComponent<Canvas> ().enabled = false;
+        Unpause();
     }
 
     public void restartPress()

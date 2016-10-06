07fb069c86b8d4f123c3d4abecca58792815b2a3
Hanzhi Wang <hanzhi2@gmail.com>
2016-10-06 22:02:53 +1300

    clamped tank angle to -45 to 45 degrees to prevent tank flipping. resolves #8

--- Assets/Scripts/Player/TankController.cs
+++ Assets/Scripts/Player/TankController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TankController : MonoBehaviour {
     public UnityEngine.GameObject tankBase;
-    public float acceleration, drag, topSpeed;
+    public float acceleration, drag, topSpeed, angleLimit;
     public bool canMove = true;
     
     private float speed;
@@ -42,7 +42,7 @@ public class TankController : MonoBehaviour {
 			rb.AddForce (-tankBase.transform.forward * acceleration, ForceMode.Acceleration);
 		}
 
-		//Calculate the horizontal speed of the tank (how much it's 'sliding')
+		//Calculate the horizontal speed of the tank (how much it's 'sliding') and apply drag in that direction
 		float horizontalSpeed=Vector3.Project (rb.velocity, tankBase.transform.right).magnitude;
 		if (Vector3.Angle (rb.velocity, tankBase.transform.right) > 90) {
 			horizontalSpeed *= -1;
@@ -51,7 +51,37 @@ public class TankController : MonoBehaviour {
 		if (Mathf.Abs(horizontalSpeed) > 1) {
 			rb.AddForce (tankBase.transform.right * horizontalSpeed * -drag, ForceMode.Acceleration);
 		}
-	}
+
+        //check if angle with ground plane is >45 degrees, clamp rotation to between -45 and 45 degrees with ground
+        // in both x and z axes
+        if (transform.rotation.eulerAngles.x > angleLimit && transform.rotation.eulerAngles.x < 360 - angleLimit) {
+            if (transform.rotation.eulerAngles.x < 180) {
+                transform.rotation = Quaternion.Euler(
+                    angleLimit,
+                    transform.rotation.eulerAngles.y,
+                    transform.rotation.eulerAngles.z);
+            } else {
+                transform.rotation = Quaternion.Euler(
+                    360 - angleLimit,
+                    transform.rotation.eulerAngles.y,
+                    transform.rotation.eulerAngles.z);
+            }
+        }
+
+        if (transform.rotation.eulerAngles.z > angleLimit && transform.rotation.eulerAngles.z < 360 - angleLimit) {
+            if (transform.rotation.eulerAngles.z < 180) {
+                transform.rotation = Quaternion.Euler(
+                    transform.rotation.eulerAngles.x,
+                    transform.rotation.eulerAngles.y,
+                    angleLimit);
+            } else {
+                transform.rotation = Quaternion.Euler(
+                    transform.rotation.eulerAngles.x,
+                    transform.rotation.eulerAngles.y,
+                    360 - angleLimit);
+            }
+        }
+    }
 
 
 	//These are called on Update, so set flags to defer actions until FixedUpdate

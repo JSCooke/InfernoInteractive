8ec9dc1257b2f582e171b3bfcec59b5a87793955
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-10 21:44:51 +1300

    Added boxes with rigidbodies
    Box rotation is restricted and movement only in x and z directions

---
+++ Assets/Scripts/BoxPuzzle/BoxBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxBehaviour : MonoBehaviour
+
+{
+	private bool isMoving = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY 
+			| RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionY;
+
+	}
+}

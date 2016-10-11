98b73ad64781b837ab1146aeea5dba8974d01d85
Isabel Zhuang <izhu678@aucklanduni.ac.nz>
2016-10-11 23:02:24 +1300

    Circular countdown bar timer implemented and functional

---
+++ Assets/CircularBarScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CircularBarScript : MonoBehaviour {
+
+    [SerializeField]
+    private Image circularBar;
+
+	// Use this for initialization
+	void Start () {
+        circularBar.fillAmount = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        circularBar.fillAmount = circularBar.fillAmount - (float)0.1*Time.deltaTime;
+	}
+}

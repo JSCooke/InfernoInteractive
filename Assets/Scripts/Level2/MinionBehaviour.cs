1d4e8ee50a7d60af47d2faf15f69b8fd5e02fb81
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-11 17:14:04 +1300

    Made minions spawn and killable
    Minions have health and take damage

---
+++ Assets/Scripts/Level2/MinionBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionBehaviour : MonoBehaviour
+{
+
+	public UnityEngine.GameObject player = null;
+
+	private Rigidbody rb;
+	private int currentHealth, maxHealth;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		maxHealth = this.GetComponent<BossController>().maxHealth;
+		currentHealth = maxHealth;
+
+		if (player == null)
+		{
+			player = UnityEngine.GameObject.FindGameObjectsWithTag("Player")[0];
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		currentHealth = this.GetComponent<BossController>().currentHealth;
+
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+
+		string collidedTag = collision.gameObject.tag;
+		if (collidedTag == "Player" || collidedTag == "Enemy")
+		{
+			//dashing = false;
+			//finding = true;
+		}
+	}
+
+	void Die()
+	{
+		Destroy(this.gameObject);
+	}
+}

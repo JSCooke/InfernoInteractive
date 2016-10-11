fbf93399623db2cc55bba2014f929d0ba98aaebe
Isabel Zhuang <izhu678@aucklanduni.ac.nz>
2016-10-11 22:02:42 +1300

    Minion can pace left and right using specified input offset

---
+++ Assets/MinionBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionBehaviour : MonoBehaviour {
+
+    public float offset;
+    private Rigidbody rb;
+    private Vector3 originalPosition;
+    private bool moveLeft;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        originalPosition = transform.position;
+	}
+    
+    void Update () {
+
+        //Checks if the current position has moved further than the offset
+        //If so, move in the opposite direction
+        if ((transform.position - originalPosition).magnitude > offset)
+        {
+            moveLeft = !moveLeft;
+        }
+            
+        //Move left or right depending on the boolean set above
+        if(moveLeft)
+        {
+            transform.Translate(Vector3.left * 5f * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * 5f * Time.deltaTime);
+        }
+            
+	}
+}

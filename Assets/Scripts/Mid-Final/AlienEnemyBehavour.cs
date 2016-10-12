7864227cb967ee9dba8db7a26706efa92d454ab8
Kristy Tsoi <kris.t.tsoi@gmail.com>
2016-10-12 22:38:49 +1300

    make shots dissappear when hit enemy

--- Assets/Scripts/Mid-Final/AlienEnemyBehavour.cs
+++ Assets/Scripts/Mid-Final/AlienEnemyBehavour.cs
@@ -1,35 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AlienEnemyBehavour : MonoBehaviour {
+public class AlienEnemyBehavour : Damageable{
 
     UnityEngine.GameObject tank;
     Transform emp;
     NavMeshAgent navagation;
     bool toEMP = true;
 
+    CapsuleCollider capsuleCollider;
+
+    float timeBetweenAttacks = 0.5f;
+    float timer;
+    public int attackDamage;
+
+    bool inRange = false;
+
 
     // Use this for initialization
     void Start () {
-
 
         if (tank == null)
         {
             tank = UnityEngine.GameObject.FindGameObjectsWithTag("Player")[0];
         }
-
         emp = GameObject.FindGameObjectWithTag("EMP").transform;
-
         navagation = GetComponent<NavMeshAgent>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        maxHealth = 50;
+        currentHealth = maxHealth;
 
         //depending on difficulty enemies spawn faster
         //easy 2 enemies every 3 sec
         //med 4 enemies every 3 sec
         //hard 6 enemies every 3 sec
 
-
-        //Either attack emp or attack tank depending on which is closer?
-        //attack emp unless tank is within a certain range
+        
 
     }
 	
@@ -43,6 +50,13 @@ public class AlienEnemyBehavour : MonoBehaviour {
         else
         {
             navagation.SetDestination(tank.transform.position);
+        }
+
+        timer += Time.deltaTime;
+
+        if(timer>=timeBetweenAttacks && inRange && currentHealth > 0)
+        {
+
         }
         
 
@@ -75,6 +89,39 @@ public class AlienEnemyBehavour : MonoBehaviour {
             //attack player
         }
         //else if it is a bullet then destroy
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "PlayerProjectile")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+
+    public void TakeDamage (int amount)
+    {
+        currentHealth -= amount;
+
+        //if dead
+        if (currentHealth <= 0)
+        {
+            //turn to trigger so shots can pass through
+            capsuleCollider.isTrigger = true;
+        }
+    }
+
+    void Attack()
+    {
+        timer = 0f;
+        if(currentHealth > 0)
+        {
+            //tank.takeDamage(attackDamage);
+            //TODO take damage
+        }
+
     }
 
 }

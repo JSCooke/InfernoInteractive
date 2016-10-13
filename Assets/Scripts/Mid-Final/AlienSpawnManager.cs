533d3c9c55391cf7db9586e763027afcf75975ad
Kristy Tsoi <kris.t.tsoi@gmail.com>
2016-10-13 22:26:08 +1300

    can spawn enemies

--- Assets/Scripts/Mid-Final/AlienSpawnManager.cs
+++ Assets/Scripts/Mid-Final/AlienSpawnManager.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class AlienSpawnManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public TankController tank;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    // Use this for initialization
+    void Start () {
+
+        //repeat method,start time, time before call again
+        InvokeRepeating("Spawn", 0, spawnTime);
 	
 	}
+
+    void Spawn()
+    {
+        //if tank health is not 0 then
+
+        int spawnPointIndex = Random.RandomRange(0, spawnPoints.Length);
+
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
 	
-	// Update is called once per frame
-	void Update () {
 	
-	}
 }

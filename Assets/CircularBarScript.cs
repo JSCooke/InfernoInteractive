6e1dc11ff8785de670ddbb1b1129ae2d2fef186e
Isabel Zhuang <izhu678@aucklanduni.ac.nz>
2016-10-13 13:36:40 +1300

    Robots health bars appear and countdown when entering trigger field

--- Assets/CircularBarScript.cs
+++ Assets/CircularBarScript.cs
@@ -7,26 +7,39 @@ public class CircularBarScript : MonoBehaviour {
     public GameObject enemy;
     public Image circularBar;
     public float speed;
-	public GameObject countdownBarCanvas;
+	public Canvas countdownBarCanvas;
+    public bool startCountdown;
 
     void Start () {
         circularBar.fillAmount = 1;
-	}
+        circularBar.color = Color.yellow;
+    }
 
 	void Update () {
-        //Change colour frome green to red
-        circularBar.color = Color.Lerp(Color.red, Color.yellow, circularBar.fillAmount);
-
-        //Make health bar empty/unfill to representing a timer running out
-        circularBar.fillAmount = circularBar.fillAmount - speed*Time.deltaTime;
+        
+		if (startCountdown) {
+            StartCountdownHealthBar();
+        }
 
         //Make the health bar appear below the specified enemy
         Vector3 newPosition = enemy.transform.position;
         newPosition.y = (float)1.5;
         transform.position = Camera.main.WorldToScreenPoint(newPosition);
 
-		if (circularBar.fillAmount == 0) {
-			countdownBarCanvas.SetActive (false);
-		}
+        //Reset all values when health bar runs out
+        if (circularBar.fillAmount == 0)
+        {
+            countdownBarCanvas.GetComponent<Canvas>().enabled = false;
+            ((CircularBarScript)countdownBarCanvas.GetComponentInChildren(typeof(CircularBarScript))).startCountdown = false;
+        }
+    }
+
+    void StartCountdownHealthBar()
+    {
+        //Change colour frome green to red
+        circularBar.color = Color.Lerp(Color.red, Color.yellow, circularBar.fillAmount);
+
+        //Make health bar empty/unfill to representing a timer running out
+        circularBar.fillAmount = circularBar.fillAmount - speed * Time.deltaTime;
     }
 }

0046f394954f5aeb8aa7ae779b772ddb95e92348
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-13 16:58:43 +1300

    Fixed merging issues, fixed teleporter and added textures

--- Assets/Scripts/Level2/TeleportController.cs
+++ Assets/Scripts/Level2/TeleportController.cs
@@ -11,6 +11,10 @@ public class TeleportController : MonoBehaviour {
 		if (other.gameObject.tag == "Player")
 		{
 			other.transform.position = new Vector3(gotoX, gotoY, gotoZ);
+			other.transform.rotation = Quaternion.identity;
+			other.transform.Find("Treads").gameObject.transform.rotation = Quaternion.identity;
+			other.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+
 		}
 	}
 	

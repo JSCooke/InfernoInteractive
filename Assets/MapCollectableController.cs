ddc95838d152b6dba799d3c666d6a05b2f14243a
Rebecca Lee <rlee291@aucklanduni.ac.nz>
2016-10-14 16:27:36 +1300

    Added win screen

---
+++ Assets/MapCollectableController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapCollectableController : MonoBehaviour {
+
+	public int id;
+
+	public void OnTriggerEnter ()
+	{
+		// TODO "pick up" map
+
+		if (id == 3) // last map to collect
+		{
+			UIAdapter.win();
+		}
+	}
+
+}

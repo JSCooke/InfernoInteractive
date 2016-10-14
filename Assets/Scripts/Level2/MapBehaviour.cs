f1211e05c8b302e1fb9036c85241ecf087d47280
sabflik <szaf045@aucklanduni.ac.nz>
2016-10-14 16:46:12 +1300

    Added coordinate map and fixed hole prefab

---
+++ Assets/Scripts/Level2/MapBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapBehaviour : MonoBehaviour {
+
+	public float rotateSpeed = 1;
+	public float translateSpeed = 1;
+	public float translateAmplitude = 1;
+
+	private Vector3 _startPosition;
+
+	// Use this for initialization
+	void Start () {
+		_startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, 20 * Time.deltaTime * rotateSpeed);
+
+		transform.position = _startPosition + new Vector3(0.0f, translateAmplitude * Mathf.Sin(Time.time * translateSpeed), 0.0f);
+	}
+}

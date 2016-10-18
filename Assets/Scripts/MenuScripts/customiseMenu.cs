564564537dc2b4c2ff478e972caf382449ccf11e
Isabel Zhuang <izhu678@aucklanduni.ac.nz>
2016-10-18 22:30:43 +1300

    Customise menu changes to and from larger menu screen

--- Assets/Scripts/MenuScripts/customiseMenu.cs
+++ Assets/Scripts/MenuScripts/customiseMenu.cs
@@ -5,15 +5,20 @@ using UnityEngine.SceneManagement;
 public class customiseMenu : MonoBehaviour {
 
 	public Canvas mainMenu;
+    public new GameObject camera;
+    private MeshRenderer[] menus;
     /*public GameObject[] AttackUpgrades = new GameObject[2];
     public GameObject[] DefenseUpgrades = new GameObject[1];
     public GameObject[] MovementUpgrades = new GameObject[0];
     public GameObject[] LeftUpgrades = new GameObject[2];
-    public GameObject[] RightUpgrades = new GameObject[2];
+    public GameObject[] RightUpgrades = new GameObject[2];*/
 
     void Start()
     {
-        for (int i = 0; i < AttackUpgrades.Length; i++)
+        //Start menu is the in the second index
+        menus = camera.GetComponentsInChildren<MeshRenderer>();
+
+        /*for (int i = 0; i < AttackUpgrades.Length; i++)
         {
             AttackUpgrades[i].SetActive(false);
         }
@@ -32,15 +37,17 @@ public class customiseMenu : MonoBehaviour {
         for (int i = 0; i < RightUpgrades.Length; i++)
         {
             RightUpgrades[i].SetActive(false);
-        }
-    }*/
+        }*/
+    }
 
-	public Camera tankCamera;
+    public Camera tankCamera;
 
     public void BackPress()
     {
 		this.GetComponent<Canvas> ().enabled = false;
 		mainMenu.enabled = true;
+        menus[0].enabled = false;
+        menus[1].enabled = true;
     }
 
 	void Update(){

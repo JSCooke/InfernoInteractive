19f67a8d21f0142778ee8098a5f2dba5dd1073bd
Jordan <jwon223@aucklanduni.ac.nz>
2016-10-18 17:17:16 +1300

    intergrated shockwave with boss

--- Assets/Scripts/Boss/FinalBoss/Skills/Slam.cs
+++ Assets/Scripts/Boss/FinalBoss/Skills/Slam.cs
@@ -5,26 +5,14 @@ public class Slam : MonoBehaviour {
 
     public string playerName = "Tank";
     public float speed = 1f;
-    private ParticleSystem shockwave;
 
 	// Use this for initialization
 	void Start () {
-
-        ParticleSystem lol = this.GetComponentsInChildren<ParticleSystem>()[0];
-        
-        lol.startRotation = Mathf.Deg2Rad * 90;
     }
 
     // Update is called once per frame
     void Update() {
-        Vector3 relativePos = GameObject.Find(playerName).transform.position - this.transform.position;
-        Quaternion rotation = Quaternion.LookRotation(relativePos);
-        ParticleSystem lol = this.GetComponentsInChildren<ParticleSystem>()[0];
-        lol.startRotation = Mathf.Deg2Rad * rotation.eulerAngles.y;
-        lol.startLifetime = 1;
-        lol.startSize = 30;
-        lol.Play();
-        //transform.position = Vector3.MoveTowards(transform.position, GameObject.Find(playerName).transform.position, speed);
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find(playerName).transform.position, speed);
     }
 
 }

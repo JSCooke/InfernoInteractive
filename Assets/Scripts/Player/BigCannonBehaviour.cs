2e63fb51609f054a03e42097eceeb10523e7cad7
Hanzhi Wang <hanzhi2@gmail.com>
2016-10-21 03:08:33 +1300

    tank clone no longr spawns projectiles when shooting

--- Assets/Scripts/Player/BigCannonBehaviour.cs
+++ Assets/Scripts/Player/BigCannonBehaviour.cs
@@ -7,16 +7,19 @@ public class BigCannonBehaviour : ControlStationBehaviour {
     public int cooldown;
 	public float rotationSpeed;
 
+    private bool isClone;
+
 	void Start(){
 		//Auto-find tank references on start
 		tankController = GetComponentInParent<TankController> ();
 		tank = tankController.gameObject;
 		cabin = tank.transform.Find ("Cabin").gameObject;
+        isClone = GetComponentInParent<TankCloner>().isClone;
 	}
 
 	private int lastShotTime;
     public override void keyPressed(bool up, bool left, bool down, bool right) {
-        if (up && Time.frameCount - lastShotTime > cooldown) {
+        if (up && Time.frameCount - lastShotTime > cooldown && !isClone) {
             lastShotTime = Time.frameCount;
 			SoundAdapter.playCannonMk1Sound ();
             Instantiate(projectile, projectileSpawn.transform.position, projectileSpawn.transform.rotation);
